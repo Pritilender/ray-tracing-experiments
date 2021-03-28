85ff9474f6a0dda28db7393257980f61524c4847
Mihajlo Ilijic <sajn_ap@live.com>
2021-03-28 21:17:50 +0200

    Add simple ppm rendering to file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello World")
+import { writeFile } from "fs/promises"
+
+const main = async () => {
+  const imageWidth = 256
+  const imageHeight = 256
+
+  let fileContent = `P3\n${imageWidth} ${imageHeight}\n255\n`
+
+  for (let j = imageHeight - 1; j >= 0; j--) {
+    console.info(`\rScanlines remaining: ${j}`)
+    for (let i = 0; i < imageWidth; i++) {
+      const r = i / (imageWidth - 1)
+      const g = j / (imageHeight - 1)
+      const b = 0.25
+
+      const ir = (255 * r) | 0
+      const ig = (255 * g) | 0
+      const ib = (255 * b) | 0
+
+      fileContent += `${ir} ${ig} ${ib}\n`
+    }
+  }
+
+  await writeFile("img/out.ppm", fileContent)
+  console.info("\nDone.\n")
+}
+
+main().then(_ => console.log("Bye!"))
